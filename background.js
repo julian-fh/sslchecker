0985d8a052470a08db8a4b4cae99c87cf6e2877a
Julian Frielinghaus <julian.frielinghaus@tum.de>
2017-04-30 01:38:51 +0200

    - added host extraction

--- background.js
+++ background.js
@@ -1,14 +1,25 @@
-var pattern = "https://*/*";
+var pattern = "https://*.google.de/*";
 
 function redirect(requestDetails) {
   console.log("Redirecting: " + requestDetails.url);
   return {
-    redirectUrl: "https://google.de"
+    redirectUrl: "https://facebook.de"
   };
 }
 
+
+function getHostname(href) {
+    var l = document.createElement("a");
+    l.href = href.url;
+    var hostname = l.hostname;
+    var res = hostname.split(".");
+    var host = res[res.length - 2];
+    
+    console.log(host);
+}
+
+
 browser.webRequest.onBeforeRequest.addListener(
-  redirect,
-  {urls:[pattern]},
-  ["blocking"]
+  getHostname,
+  {urls:[pattern]}
 );
